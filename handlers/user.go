c395ce5491bbe1c4e14fca901ff14244e14e2466
anit rai <anit.rai@cyphertree.com>
2017-06-11 12:56:54 +0530

    fix typo

--- handlers/user.go
+++ handlers/user.go
@@ -82,7 +82,7 @@ func Facebook(c *gin.Context) {
 			Email:     fb.Email,
 		}
 		// user created, now start scraping data
-		err = facebook.SaveInitFeed(&user.Fb)
+		err = facebook.FetchFeed(user, 0)
 		if err != nil {
 			panic(err)
 		}
